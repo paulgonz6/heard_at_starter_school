a9a2034566b356770b07c391bb527ab0763e4a50
Paul Gonzalez <pjg5005@gmail.com>
2015-05-21 13:28:12 -0500

    Adds users model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150521174303) do
+ActiveRecord::Schema.define(version: 20150521182512) do
 
   create_table "quotes", force: :cascade do |t|
     t.string   "text"
@@ -20,6 +20,16 @@ ActiveRecord::Schema.define(version: 20150521174303) do
     t.string   "speaker"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "provider"
+    t.string   "uid"
+    t.string   "name"
+    t.string   "screen_name"
+    t.string   "image_url"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
 end
